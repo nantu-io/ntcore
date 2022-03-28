2a2691359823988700446f3561d0fbba677dd78a
dsp-columbus <dsp.columbus.2018@gmail.com>
2022-03-28 11:23:29 -0400

    Upgrade python sdk version

--- sdk/python/ntcore/__about__.py
+++ sdk/python/ntcore/__about__.py
@@ -1,8 +1,12 @@
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 __version__ = "0.0.3"
 =======
 __version__ = "0.0.8"
 >>>>>>> Stashed changes
+=======
+__version__ = "0.0.4"
+>>>>>>> 4495ced (Upgrade python sdk version)
 __description__ = "Python client for interfacing with NTCore"
 __license__ = "Apache 2.0"
 __maintainer__ = "NTCore"
