8532bd38861c76f0de07455cce42e000e9f7b79c
dsp-columbus <dsp.columbus.2018@gmail.com>
2022-03-31 23:17:44 -0400

    Add required dependencies for python sdk

--- sdk/python/ntcore/__about__.py
+++ sdk/python/ntcore/__about__.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 __version__ = "0.0.3"
 =======
@@ -7,6 +8,9 @@ __version__ = "0.0.8"
 =======
 __version__ = "0.0.4"
 >>>>>>> 4495ced (Upgrade python sdk version)
+=======
+__version__ = "0.0.5"
+>>>>>>> 3bc81d9 (Add required dependencies for python sdk)
 __description__ = "Python client for interfacing with NTCore"
 __license__ = "Apache 2.0"
 __maintainer__ = "NTCore"
