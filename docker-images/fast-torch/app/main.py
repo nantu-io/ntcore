56c6620862c2339625f002e0a68d945405d61f23
dsp-columbus <dsp.columbus.2018@gmail.com>
2022-05-08 08:44:07 -0400

    Use NTCore client and monitor in fast-torch

--- docker-images/fast-torch/app/main.py
+++ docker-images/fast-torch/app/main.py
@@ -1,45 +1,55 @@
-import os, logging, requests
+import os, logging, tempfile
 from fastapi import FastAPI
 from models import Request
 from ts.context import Context
 from util import build_context, get_torch_handler
-import tempfile
+from ntcore import Client
+from ntcore.monitor import Monitor
+import time
 
-template = "http://{endpoint}/dsp/api/v1/workspace/{workspaceId}/model/{version}"
-model_file = "model.pt"
-try:
-    endpoint=os.environ["DSP_API_ENDPOINT"]
-    workspace_id=os.environ["DSP_WORKSPACE_ID"]
-    version=os.environ["DSP_MODEL_VERSION"]
-    url = template.format(endpoint=endpoint, workspaceId=workspace_id, version=version)
-    # Read serialized model from NTCore server.
-    logging.info('Downloading model from {0}'.format(url))
-    serialized_model = requests.get(url).content
-    
-    # Write serialized model to a binary file.
-    model_dir = tempfile.TemporaryDirectory()
-    f = open(os.path.join(model_dir.name, model_file), 'wb')
-    f.write(serialized_model)
-    f.close()
-except Exception as e:
-    logging.error("Unable to load model: {0}".format(str(e))) 
+# Config the enviroment
+workspace_id = os.environ["DSP_WORKSPACE_ID"]
+model_dir = tempfile.TemporaryDirectory()
+
+# Initialize NTCore client.
+client = Client(server="http://" + os.environ["DSP_API_ENDPOINT"])
+monitor = Monitor(server="http://" + os.environ["DSP_API_ENDPOINT"])
+
+# Download serialized model
+client.download_model(os.path.join(model_dir.name, "model.pt"), workspace_id)
+
+# Build inference context.
+context: Context = build_context(workspace_id, model_dir.name, "model.pt")
+
+# Initialize torch handler cache
+torch_handlers = dict()
 
 # Initialize Fast API server.
 app = FastAPI()
+
 
 @app.post('/predict')
 async def predict(request: Request):
     """
     Returns predictions based on the input data and selected torch handler.
     """
-    torch_handler = get_torch_handler(request.handler)
-    context: Context = build_context(workspace_id, model_dir.name, model_file)
-    try:
+    if (request.handler not in torch_handlers):
+        torch_handler = get_torch_handler(request.handler)
         torch_handler.initialize(context)
-        return torch_handler.handle(request.data, context)
+        torch_handlers[request.handler] = torch_handler
+    else:
+        torch_handler = torch_handlers[request.handler]
+
+    try:
+        start_time = round(time.time() * 1000)
+        prediction = torch_handler.handle(request.data, context)
+        monitor.add_metric(workspace_id, "Latency", round(time.time() * 1000) - start_time)
+        return prediction
     except Exception as e:
+        monitor.add_metric(workspace_id, "Error", 1.0)
         logging.error('Unable to get prediction: {0}'.format(str(e)))
         return {"error": str(e)}
+
 
 @app.get("/health")
 async def healthcheck():
