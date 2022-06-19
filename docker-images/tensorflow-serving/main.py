35b83ef4d1e34b4fee582460e46382640d54e778
dsp-columbus <dsp.columbus.2018@gmail.com>
2022-06-18 21:06:04 -0400

    Fix tensorflow model path issue

--- docker-images/tensorflow-serving/main.py
+++ docker-images/tensorflow-serving/main.py
@@ -1,26 +1,27 @@
 from pathlib import Path
-import logging
-import tarfile
-import os
 from ntcore import Client
+import logging, tarfile, os
 
-MODEL_BASEPATH = "./models"
-MODEL_FILENAME = "model.tar.gz"
-url = "http://" + os.environ["DSP_API_ENDPOINT"]
-client = Client(server="http://" + os.environ["DSP_API_ENDPOINT"])
+workspace_id = os.environ["DSP_WORKSPACE_ID"]
+endpoint = os.environ["DSP_API_ENDPOINT"]
+url = endpoint if endpoint.startswith("http://") else "http://" + endpoint
+# Initialize ntcore client with given server url
+client = Client(server=url)
+# tensorflow-serving requires version in number format
+model_version = "1"
 
 try:
-    extract_path = os.path.join(MODEL_BASEPATH, os.environ["DSP_WORKSPACE_ID"])
-    model_path = os.path.join(extract_path, MODEL_FILENAME)
+    extract_path = os.path.join("/models", workspace_id)
+    model_path = os.path.join(extract_path, "model.tar.gz")
     Path(extract_path).mkdir(parents=True, exist_ok=True)
     # download model from ntcore
-    client.download_model(model_path, os.environ["DSP_WORKSPACE_ID"])
+    client.download_model(model_path, workspace_id)
     # Unzip the file to get the original saved model
     with tarfile.open(model_path, "r:gz") as tar:
         tar.extractall(extract_path)
     # Remove the binary model file
     os.remove(model_path)
-    os.rename(os.path.join(extract_path, 'model'), os.path.join(extract_path, os.environ["DSP_MODEL_VERSION"]))
+    os.rename(os.path.join(extract_path, 'model'), os.path.join(extract_path, model_version))
 
 except Exception as e:
     logging.error("Failed loading model from {0}: {1}".format(url, e)) 
