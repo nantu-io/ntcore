6f246c79a7f05d8a83ac79670d1223b57ae97c1f
dsp-columbus <dsp.columbus.2018@gmail.com>
2022-08-17 20:49:31 -0400

    Enable proxy for tensorflow serving

--- docker-images/tensorflow-serving/docker-entrypoint.sh
+++ docker-images/tensorflow-serving/docker-entrypoint.sh
@@ -1,14 +1,5 @@
 #! /usr/bin/env sh
 
-if [ -z "${DSP_API_ENDPOINT}" ]; then
-    echo "DSP_API_ENDPOINT not set."
-    exit
-fi
-if [ -z "${DSP_WORKSPACE_ID}" ]; then
-    echo "DSP_WORKSPACE_ID not set."
-    exit
-fi
-
 # Execute python script to download and deserialize model
 python3 /usr/local/bin/main.py
 
--- docker-images/tensorflow-serving/main.py
+++ docker-images/tensorflow-serving/main.py
@@ -1,28 +1,36 @@
+import logging, tarfile, os
 from pathlib import Path
 from ntcore import Client
-import logging, tarfile, os
 
-workspace_id = os.environ["DSP_WORKSPACE_ID"]
-endpoint = os.environ["DSP_API_ENDPOINT"]
-url = endpoint if endpoint.startswith("http://") else "http://" + endpoint
-# Initialize ntcore client with given server url
-client = Client(server=url)
-# tensorflow-serving requires version in number format
-model_version = "1"
+def download_model():
+    """
+    Downloads model from ntcore server and place in the serving path.
+    """
+    # Fetches environment variables
+    workspace_id = os.environ["DSP_WORKSPACE_ID"]
+    api_endpoint = os.environ["DSP_API_ENDPOINT"]
+    # Initialize ntcore client with given server url
+    ntcore_server = api_endpoint if api_endpoint.startswith("http://") else "http://" + api_endpoint
+    ntcore_client = Client(server=ntcore_server)
 
-try:
-    extract_path = os.path.join("/models", workspace_id)
-    model_path = os.path.join(extract_path, "model.tar.gz")
-    Path(extract_path).mkdir(parents=True, exist_ok=True)
-    # download model from ntcore
-    client.download_model(model_path, workspace_id)
-    # Unzip the file to get the original saved model
-    with tarfile.open(model_path, "r:gz") as tar:
-        tar.extractall(extract_path)
-    # Remove the binary model file
-    os.remove(model_path)
-    os.rename(os.path.join(extract_path, 'model'), os.path.join(extract_path, model_version))
+    try:
+        extract_path = os.path.join("/models", workspace_id)
+        model_path = os.path.join(extract_path, "model.tar.gz")
+        Path(extract_path).mkdir(parents=True, exist_ok=True)
+        # download model from ntcore
+        ntcore_client.download_model(model_path, workspace_id)
+        # Unzip the file to get the original saved model
+        with tarfile.open(model_path, "r:gz") as tar:
+            tar.extractall(extract_path)
+        # Remove the binary model file
+        os.remove(model_path)
+        # tensorflow-serving requires version in number format
+        os.rename(os.path.join(extract_path, 'model'), os.path.join(extract_path, "1"))
 
-except Exception as e:
-    logging.error("Failed loading model from {0}: {1}".format(url, e)) 
-    err_message = "Error: " + str(e)
+    except Exception as e:
+        logging.error("Failed loading model from {0}: {1}".format(ntcore_server, e))
+
+
+if __name__ == '__main__':
+    # Downloads model from ntcore server and place in the serving path.
+    download_model()
