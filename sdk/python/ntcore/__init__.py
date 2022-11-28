399122a29d0ad83b98709ca9899d26fa1a38b5ce
dsp-columbus <dsp.columbus.2018@gmail.com>
2022-11-27 21:29:27 -0500

    Remove TorchModelRecorder from ntcore __init__

--- sdk/python/ntcore/__init__.py
+++ sdk/python/ntcore/__init__.py
@@ -1,6 +1,5 @@
 from ntcore.client import Client
 from importlib import import_module
-from ntcore.integrations.torch import TorchModelRecorder
 
 for module in ["ntcore.integrations.sklearn", "ntcore.integrations.tensorflow", "ntcore.integrations.torch"]:
     try:
